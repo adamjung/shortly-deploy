e8083b17fcbcc98bced9c71b891213ca8bfabb8f
Hack Reactor Students <accounts@hackreactor.com>
2016-11-30 20:55:49 -0800

    attempt to fix bug in grunt concat

--- Gruntfile.js
+++ Gruntfile.js
@@ -8,7 +8,7 @@ module.exports = function(grunt) {
       },
       dist: {
         src: ['public/**/*.js'],
-        dest: 'public/dist/compiled.js',
+        dest: 'public/compiled.js',
       }
     },
 
