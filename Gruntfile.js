8215cd7d69143bd2724b1b1086bd7cbb33e393c3
Hack Reactor Students <accounts@hackreactor.com>
2016-12-01 15:27:51 -0800

    whatevs

--- Gruntfile.js
+++ Gruntfile.js
@@ -78,7 +78,7 @@ module.exports = function(grunt) {
 
     shell: {
       remotePush: {
-        command: 'git add . \n git commit -m \' whatevs \' \n git push live master'
+        command: 'git add . \n git commit -m \'whatevs \'\ngit push live master'
       }
     },
   });
